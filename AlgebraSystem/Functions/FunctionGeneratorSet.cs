52fbcee0d9af5242b5437acd451135b96e176bca
Joe O'Connor <joeoc2625@gmail.com>
2020-11-01 00:29:52 +0000

    Added better seperation between Function Identities and Function Generators

--- AlgebraSystem/Functions/FunctionGeneratorSet.cs
+++ AlgebraSystem/Functions/FunctionGeneratorSet.cs
@@ -6,7 +6,7 @@ using Algebra.Functions.FunctionIdentities;
 
 namespace Algebra.Functions
 {
-    public class FunctionGeneratorSet : IEnumerable<FunctionGenerator>
+    public class FunctionGeneratorSet : IEnumerable<FunctionGenerator>, IDictionary<string, FunctionGenerator>
     {
         private static readonly FunctionGenerator _sinFunctionGenerator = new AtomicFunctionGenerator("sin", 1, list => Expression.SinOf(list[0]));
         private static readonly FunctionGenerator _arcsinFunctionGenerator = new AtomicFunctionGenerator("arcsin", 1, list => Expression.ArcsinOf(list[0]));
@@ -14,29 +14,19 @@ namespace Algebra.Functions
         private static readonly FunctionGenerator _lnFunctionGenerator = new AtomicFunctionGenerator("ln", 1, list => Expression.LnOf(list[0]));
         private static readonly FunctionGenerator _signFunctionGenerator = new AtomicFunctionGenerator("sign", 1, list => Expression.SignOf(list[0]));
 
-        public static readonly FunctionGeneratorSet DefaultFunctions = new FunctionGeneratorSet()
+        public static readonly FunctionGeneratorSet _defaultFunctionGenerators = new FunctionGeneratorSet(FunctionIdentitySet.DefaultFunctionIdentities)
         {
             { _sinFunctionGenerator },
-            { CosIdentity.Instance },
-            { TanIdentity.Instance },
             { _arcsinFunctionGenerator },
-            { ArccosIdentity.Instance },
             { _arctanFunctionGenerator },
             { _lnFunctionGenerator },
-            { LogIdentity.Instance },
             { _signFunctionGenerator },
-            { MaxIdentity.Instance },
-            { MinIdentity.Instance },
-            { SelectIdentity.Instance },
-            { AbsIdentity.Instance },
-            { SinhIdentity.Instance },
-            { CoshIdentity.Instance },
-            { TanhIdentity.Instance },
-            { ArsinhIdentity.Instance },
-            { ArcoshIdentity.Instance },
-            { ArtanhIdentity.Instance },
-            { SqrtIdentity.Instance },
         };
+
+        public static FunctionGeneratorSet DefaultFunctionGenerators
+        {
+            get => new FunctionGeneratorSet(_defaultFunctionGenerators);
+        }
 
         private readonly Dictionary<string, FunctionGenerator> _generators;
 
@@ -54,11 +44,29 @@ namespace Algebra.Functions
 
         public FunctionGeneratorSet(IDictionary<string, FunctionGenerator> generators)
         {
-            this._generators = new Dictionary<string, FunctionGenerator>(generators);
+            _generators = new Dictionary<string, FunctionGenerator>(generators);
+        }
+
+        public FunctionGeneratorSet(IEnumerable<FunctionGenerator> generators)
+            : this()
+        {
+            foreach (var generator in generators)
+            {
+                Add(generator);
+            }
         }
 
         public ICollection<string> Names { get => _generators.Keys; }
 
+        public ICollection<string> Keys => ((IDictionary<string, FunctionGenerator>)_generators).Keys;
+
+        public ICollection<FunctionGenerator> Values => ((IDictionary<string, FunctionGenerator>)_generators).Values;
+
+        public int Count => ((ICollection<KeyValuePair<string, FunctionGenerator>>)_generators).Count;
+
+        public bool IsReadOnly => ((ICollection<KeyValuePair<string, FunctionGenerator>>)_generators).IsReadOnly;
+
+        FunctionGenerator IDictionary<string, FunctionGenerator>.this[string key] { get => ((IDictionary<string, FunctionGenerator>)_generators)[key]; set => ((IDictionary<string, FunctionGenerator>)_generators)[key] = value; }
         public FunctionGenerator this[string name]
         {
             get => _generators[name];
@@ -76,7 +84,57 @@ namespace Algebra.Functions
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return _generators.Values.GetEnumerator();
+            return _generators.GetEnumerator();
+        }
+
+        public void Add(string key, FunctionGenerator value)
+        {
+            ((IDictionary<string, FunctionGenerator>)_generators).Add(key, value);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return ((IDictionary<string, FunctionGenerator>)_generators).ContainsKey(key);
+        }
+
+        public bool Remove(string key)
+        {
+            return ((IDictionary<string, FunctionGenerator>)_generators).Remove(key);
+        }
+
+        public bool TryGetValue(string key, out FunctionGenerator value)
+        {
+            return ((IDictionary<string, FunctionGenerator>)_generators).TryGetValue(key, out value);
+        }
+
+        public void Add(KeyValuePair<string, FunctionGenerator> item)
+        {
+            ((ICollection<KeyValuePair<string, FunctionGenerator>>)_generators).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<KeyValuePair<string, FunctionGenerator>>)_generators).Clear();
+        }
+
+        public bool Contains(KeyValuePair<string, FunctionGenerator> item)
+        {
+            return ((ICollection<KeyValuePair<string, FunctionGenerator>>)_generators).Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<string, FunctionGenerator>[] array, int arrayIndex)
+        {
+            ((ICollection<KeyValuePair<string, FunctionGenerator>>)_generators).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<string, FunctionGenerator> item)
+        {
+            return ((ICollection<KeyValuePair<string, FunctionGenerator>>)_generators).Remove(item);
+        }
+
+        IEnumerator<KeyValuePair<string, FunctionGenerator>> IEnumerable<KeyValuePair<string, FunctionGenerator>>.GetEnumerator()
+        {
+            return ((IEnumerable<KeyValuePair<string, FunctionGenerator>>)_generators).GetEnumerator();
         }
     }
 }
---
+++ AlgebraSystem/Functions/FunctionIdentitySet.cs
@@ -0,0 +1,121 @@
+﻿using Algebra.Functions.FunctionIdentities;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Algebra.Functions
+{
+    public class FunctionIdentitySet : IEnumerable<FunctionIdentity>, IDictionary<string, FunctionIdentity>
+    {
+        public static readonly FunctionIdentitySet _defaultFunctionIdentities = new FunctionIdentitySet()
+        {
+            { CosIdentity.Instance },
+            { TanIdentity.Instance },
+            { ArccosIdentity.Instance },
+            { LogIdentity.Instance },
+            { MaxIdentity.Instance },
+            { MinIdentity.Instance },
+            { SelectIdentity.Instance },
+            { AbsIdentity.Instance },
+            { SinhIdentity.Instance },
+            { CoshIdentity.Instance },
+            { TanhIdentity.Instance },
+            { ArsinhIdentity.Instance },
+            { ArcoshIdentity.Instance },
+            { ArtanhIdentity.Instance },
+            { SqrtIdentity.Instance },
+        };
+
+        public static FunctionIdentitySet DefaultFunctionIdentities
+        {
+            get => new FunctionIdentitySet(_defaultFunctionIdentities);
+        }
+
+        private readonly Dictionary<string, FunctionIdentity> _identities;
+
+        public FunctionIdentitySet()
+            : this(new Dictionary<string, FunctionIdentity>())
+        {
+
+        }
+
+        public FunctionIdentitySet(IDictionary<string, FunctionIdentity> identities)
+        {
+            _identities = new Dictionary<string, FunctionIdentity>(identities);
+        }
+
+        public FunctionIdentity this[string key] { get => ((IDictionary<string, FunctionIdentity>)_identities)[key]; set => ((IDictionary<string, FunctionIdentity>)_identities)[key] = value; }
+
+        public ICollection<string> Keys => ((IDictionary<string, FunctionIdentity>)_identities).Keys;
+
+        public ICollection<FunctionIdentity> Values => ((IDictionary<string, FunctionIdentity>)_identities).Values;
+
+        public int Count => ((ICollection<KeyValuePair<string, FunctionIdentity>>)_identities).Count;
+
+        public bool IsReadOnly => ((ICollection<KeyValuePair<string, FunctionIdentity>>)_identities).IsReadOnly;
+
+        public void Add(FunctionIdentity identity)
+        {
+            _identities.Add(identity.GetName(), identity);
+        }
+
+        public void Add(string key, FunctionIdentity value)
+        {
+            ((IDictionary<string, FunctionIdentity>)_identities).Add(key, value);
+        }
+
+        public void Add(KeyValuePair<string, FunctionIdentity> item)
+        {
+            ((ICollection<KeyValuePair<string, FunctionIdentity>>)_identities).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<KeyValuePair<string, FunctionIdentity>>)_identities).Clear();
+        }
+
+        public bool Contains(KeyValuePair<string, FunctionIdentity> item)
+        {
+            return ((ICollection<KeyValuePair<string, FunctionIdentity>>)_identities).Contains(item);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return ((IDictionary<string, FunctionIdentity>)_identities).ContainsKey(key);
+        }
+
+        public void CopyTo(KeyValuePair<string, FunctionIdentity>[] array, int arrayIndex)
+        {
+            ((ICollection<KeyValuePair<string, FunctionIdentity>>)_identities).CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<KeyValuePair<string, FunctionIdentity>> GetEnumerator()
+        {
+            return ((IEnumerable<KeyValuePair<string, FunctionIdentity>>)_identities).GetEnumerator();
+        }
+
+        public bool Remove(string key)
+        {
+            return ((IDictionary<string, FunctionIdentity>)_identities).Remove(key);
+        }
+
+        public bool Remove(KeyValuePair<string, FunctionIdentity> item)
+        {
+            return ((ICollection<KeyValuePair<string, FunctionIdentity>>)_identities).Remove(item);
+        }
+
+        public bool TryGetValue(string key, out FunctionIdentity value)
+        {
+            return ((IDictionary<string, FunctionIdentity>)_identities).TryGetValue(key, out value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_identities).GetEnumerator();
+        }
+
+        IEnumerator<FunctionIdentity> IEnumerable<FunctionIdentity>.GetEnumerator()
+        {
+            return _identities.Values.GetEnumerator();
+        }
+    }
+}
